dd44f42663134ba894aa21fc6c35d6fa44fe223a
Tajay <64923628+Taj56@users.noreply.github.com>
2024-10-12 20:59:05 -0500

    implement onboarding styles
    style the onboarding screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import Icon from '@/assets/images/wordle-icon.svg'
+import { Link } from "expo-router";
+import { format } from "date-fns";
 
 export default function Index() {
   return (
@@ -11,12 +13,28 @@ export default function Index() {
         <Text style={styles.text}>Get 6 chances to guess a 5-letter word.</Text>
       </View>
 
-      <View>
-        <Text style={styles.text}>Start new game</Text>
+      <View style={styles.menu}>
+
+        <Link href={"/game"} style={[styles.btn, {backgroundColor: '#000'}]} asChild>
+          <TouchableOpacity>
+            <Text style={[styles.btnText, styles.primaryText]}>Play</Text>
+          </TouchableOpacity>
+        </Link>
+
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnText}>Log in</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnText}>Subscribe</Text>
+        </TouchableOpacity>
+
       </View>
 
       <View style={styles.footer}>
-        <Text style={styles.text}>Made by Tajay</Text>
+        <Text style={styles.footerDate}>{format(new Date(), 'MMM d, yyyy')}</Text>
+        <Text style={styles.footerText}>Made by Tajay</Text>
+        <Text style={styles.footerText}>No. 1</Text>
       </View>
 
     </View>
@@ -27,8 +45,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
-    // paddingHorizontal: 50,
+    paddingHorizontal: 50,
     gap: 40,
   },
   header: {
@@ -38,11 +55,44 @@ const styles = StyleSheet.create({
   text: {
       fontSize: 26,
       textAlign: "center",
+      fontFamily: "FrankRuhlLibre_500Medium",
   },
   title: {
       fontSize: 40,
+      fontFamily: "FrankRuhlLibre_800ExtraBold",
+  },
+  menu: {
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+  },
+  btn: {
+    justifyContent: "center",
+    borderRadius: 30,
+    alignItems: "center",
+    borderColor: "#000",
+    borderWidth: 1,
+    width: '60%',
+    maxWidth: 200
+  },
+  btnText: {
+    padding: 14,
+    fontSize: 16,
+    fontWeight: 'semibold',
+    color: '#333'
+  },
+  primaryText: {
+    color: '#fff'
   },
   footer: {
-
-  }
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  footerText: {
+    fontSize: 14,
+  },
+  footerDate: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
 })
